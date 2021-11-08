0cc7a97e8df53f9a0601ef39a08bc0db0d5e8289
Francisco Albusac <francisco.albusac@bbc.co.uk>
2021-11-08 12:28:16 +0100

    improvements

--- transform/models/silver/users_extract_cast.sql
+++ transform/models/silver/users_extract_cast.sql
@@ -1,7 +1,8 @@
 select
+    parquet_raw:id::Integer as user_id,
     parquet_raw:address::String as address,
     parquet_raw:age::Integer as age,
     parquet_raw:name::String as name,
     parquet_raw:created_at::TIMESTAMP as created_at,
     parquet_raw:updated_at::TIMESTAMP as updated_at
-from {{ source('debronze_users', 'users') }}
+from {{ source('bronze', 'users') }}
