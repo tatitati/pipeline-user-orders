65b90c35c76de00e52e7a0f9b1aa702c40098caf
Francisco Albusac <francis.jaa@protonmail.com>
2021-12-08 20:36:21 +0100

    more

---
+++ transform-snowflake/3_dedup_to_extract/dedup_to_schema.py
@@ -0,0 +1,64 @@
+#!/usr/local/bin/python3
+
+from pyspark.sql.functions import trim
+from pyspark.sql import SparkSession
+from io import BytesIO
+import boto3
+from pyspark.sql import SparkSession
+from pyspark import SparkContext
+import configparser
+import datetime
+import os
+import snowflake.connector
+from pyspark.sql.functions import col, lit
+from pyspark.sql.types import StructType, StructField, IntegerType, DateType, TimestampType, StringType
+from snowflake.connector import ProgrammingError
+import json
+
+
+jarPath='/Users/tati/lab/de/pipeline-user-orders/jars'
+jars = [
+    # spark-snowflake
+    f'{jarPath}/spark-snowflake/snowflake-jdbc-3.13.10.jar',
+    f'{jarPath}/spark-snowflake/spark-snowflake_2.12-2.9.2-spark_3.1.jar', # scala 2.12 + pyspark 3.1
+]
+os.environ['PYSPARK_SUBMIT_ARGS'] = f'--jars {",".join(jars)}  pyspark-shell'
+context = SparkContext(master="local[*]", appName="readJSON")
+app = SparkSession.builder.appName("myapp").getOrCreate()
+
+parser = configparser.ConfigParser()
+parser.read("../../pipeline.conf")
+snowflake_username = parser.get("snowflake_credentials", "username")
+snowflake_password = parser.get("snowflake_credentials", "password")
+snowflake_account_name = parser.get("snowflake_credentials", "account_name")
+SNOWFLAKE_SOURCE_NAME = "net.snowflake.spark.snowflake"
+
+snow_conn = snowflake.connector.connect(
+    user = snowflake_username,
+    password = snowflake_password,
+    account = snowflake_account_name,
+    database="mydbt",
+    schema="de_silver")
+cur = snow_conn.cursor()
+
+
+
+for table in ["USERS", "ORDERS"]:
+
+    cur.execute(f"""
+             create or replace table "MYDBT"."DE_SILVER"."{table}_EXTRACT_SCHEMA"(                
+                parquet_raw variant,                
+                schema_url varchar not null,
+                source varchar not null
+             ) as
+                select 
+                    parquet_raw,                                    
+                    parquet_raw:schema_url::varchar,                                                    
+                    source
+                from 
+                    "MYDBT"."DE_SILVER"."{table}_DEDUP";    
+            """)
+
+
+
+cur.close()
